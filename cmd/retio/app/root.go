86311a27f9cd4a8a52216d6cbbf1e710e5ed8c87
Xunzhuo <mixdeers@gmail.com>
2022-08-21 21:15:44 +0800

    chore: update licenses
    Signed-off-by: Xunzhuo <mixdeers@gmail.com>

--- cmd/retio/app/root.go
+++ cmd/retio/app/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 Xunzhuo <mixdeers@gmail.com>
+Copyright © 2022  Aeraki Authors <cncf-aerakimesh-maintainers@lists.cncf.io>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
